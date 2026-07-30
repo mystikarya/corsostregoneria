831cda6c1ca491a0ae578ce163fe0b8b28521404
mystikarya <mystikaryainfo@gmail.com>
2026-07-30 23:06:40 +0200

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -328,7 +328,7 @@ async function generateCertificate() {
 
         ctx.drawImage(image, 0, 0);
 
-        let size = 490;
+        let size = 540;
         ctx.font = `${size}px Elven`;
 
         while (
@@ -346,7 +346,7 @@ async function generateCertificate() {
         ctx.fillText(
             name,
             canvas.width / 2,
-            540
+            560
         );
 
         // Genera il PDF
